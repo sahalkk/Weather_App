9b5f56c8cac514341cf72e20dea0f9ba5cad9b4e
sahalkk <msahalkk357@gmail.com>
2022-09-03 22:10:45 +0530

    search functionality

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 console.log('hi')
 let weather = {
     apiKey : "1b0ac5363763de443cc03a609ae0aa51",
-    featchWeather : function () {
-        fetch("https://api.openweathermap.org/data/2.5/weather?q=Dubai&appid=1b0ac5363763de443cc03a609ae0aa51")
+    fetchWeather : function (cityName) {
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=1b0ac5363763de443cc03a609ae0aa51")
         .then((response) => response.json())
         .then((data) => {
             console.log(data)
@@ -13,33 +13,15 @@ let weather = {
         })
     },
     displayWeather : function(data) {
-        // const{ name } = data;
-        // const{ icon, description } = data.weather;
-        // const{ temp, humidity } = data.main;
-        // const{ speed } = data.wind;
-        // console.log(name,icon,description,temp,humidity,speed)
-        // .catch((err) => {
-        //     console.log('type error', err);
-        // })    
-        // console.log( 'sugu' + JSON.stringify(data));
-        // console.log( document.getElementById('tempValue').innerHTML);
-        document.getElementById('tempValue').innerHTML = data.main.temp;
-        document.getElementsByClassName('description')[0].innerHTML = data.weather[0].description
-        document.getElementById('humidVal').innerHTML = data.main.humidity
-        // document.getElementById('humidVal')[0].innerHTML = data.main.humidity
-        // document.getElementsById('description').innerHTML = data.weather.description;
-        // console.log('fff', document.getElementsByClassName('description')[0].innerHTML);
-        console.log('sa', document.getElementById('windVal').innerHTML = data.wind.speed);
-        
-        // console.log('fu', document.getElementById('humidVal').innerHTML = data.main.humidity)
-        // document.getElementsByClassName('humidity').innerHTML = data.main.humidity;
-        
+        document.getElementsByClassName('description')[0].innerHTML = data.weather[0].description;
+        document.getElementById('humidVal').innerHTML = data.main.humidity;                
     }
+};
 
-    
-     
-    
+
+function btnFunction(){
+    console.log('hsfd')
+    let cityName = document.getElementById('search').value 
+    weather.fetchWeather(cityName) 
+    document.getElementById('searchCity').innerText = cityName; 
 }
-
-
-weather.featchWeather()
